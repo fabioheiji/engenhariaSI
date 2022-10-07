105d32cef9cd80c1070347eff749ded5b8b97614
Fabio <fabioheiji@gmail.com>
2022-10-07 18:06:00 -0300

    feat(Create match) tests passed and feature working

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'pages#home'
-  get 'matches/:id/new'=>'matches#new'
+  resource :matches
+
 end
