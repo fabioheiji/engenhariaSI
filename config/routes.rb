f75ff271c7a288973071d6be7832965f78a67604
matheus <matheus@debian.debian>
2022-10-08 15:42:00 -0300

    Commit inicial de login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   root 'pages#home'
 
-  resources :matches
+  resources :matches, :users
 end
+
