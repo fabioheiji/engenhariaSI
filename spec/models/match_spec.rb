3c135a09ed8172d8fd4aff17e09a1c73da0f3ea5
Thais <thais.lasso@gmail.com>
2022-10-12 20:57:20 -0300

    test: unit test for search method

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -20,5 +20,22 @@ RSpec.describe Match, type: :model do
     match = Match.new
     match.level = ""
     expect(match).not_to be_valid
-  end      
+  end
+
+  it 'realiza pesquisa' do
+    match = Match.create(name: 'Rachão da EACH', description: 'Description0', address: 'USP Leste', level: "Beginner")
+    match = Match.search("EACH")
+    expect(match).to exist
+  end
+
+  it 'retorna todas as partidas' do
+    match0 = Match.create(name: 'Rachão da EACH', description: 'Description0', address: 'USP Leste', level: "Beginner") 
+    match1 = Match.create(name: 'Rachão do ICMC', description: 'Description1', address: 'Cidade Universitária', level: "Beginner")
+    match2 = Match.create(name: 'Rachão do IME', description: 'Description2', address: 'USP São Carlos', level: "Beginner")
+    
+    results = Match.search(nil)
+    matches = Match.all
+
+    expect(results).to eq(matches)
+  end
 end
