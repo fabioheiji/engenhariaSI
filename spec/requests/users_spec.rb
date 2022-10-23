d173d1fcb13481766dca2829a1b16a2a831eb1b9
Vitor Martins Cruz <vitormartinscruz.dev@gmail.com>
2022-10-23 16:52:51 -0300

    test: improve users controller tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /signup" do
+    it "returns http success" do
+      get signup_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST /signup" do
+    it "should create user account" do
+      post signup_path, params: { user: { name: "John", email: "john@email.com", password: "12345", password_confirmation: "12345", birth_date: "01/01/2001", position: "PF" } }
+      expect(response).to redirect_to(login_path)
+    end 
+
+    it "should not create user account" do
+      post signup_path, params: { user: { name: "John" } }
+      expect(response).not_to redirect_to(login_path)
+    end
   end
 end
