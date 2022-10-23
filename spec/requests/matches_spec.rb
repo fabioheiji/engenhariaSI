9c808d63afd10ddf21ebc96736c1cfadf9a5cd2d
Vitor Martins Cruz <vitormartinscruz.dev@gmail.com>
2022-10-23 17:50:53 -0300

    fix: matches test

--- spec/requests/matches_spec.rb
+++ spec/requests/matches_spec.rb
@@ -54,7 +54,7 @@ RSpec.describe "Matches", type: :request do
     it "returns http success" do
       @match = Match.create(name: 'Rachao da EACH', description: "Rachao entre alunos da Each", address: "Rua Arlindo Béttio, 1000 - Ermelino Matarazzo, São Paulo - SP, 03828-000", limit: "10", privateCourt: true, halfCourt: true, level: "livre")
       get "/matches/#{@match.id}"
-      expect(response).not_to have_http_status(:success)
+      expect(response).to have_http_status(:success)
     end
   end
 end
