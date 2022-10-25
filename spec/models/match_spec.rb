83c7a05ab786b2a7321bf1b8661ea4efc5b0b7b9
Thais Lasso <thais.lasso@gmail.com>
2022-10-25 20:27:21 -0300

    test: model now has a creator

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,25 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
+
+  before 'é válido com atributos válidos' do
+    user = User.new(
+      name: 'LeBron James', 
+      email: 'lebron@james.com', 
+      password: 'notmynameorbirthdateforsure', 
+      password_confirmation: '123456',
+      birth_date: Date.new(1984, 12, 30),
+      position: 'Power Forward'
+    )
+    user.save
+
+    @match = Match.new(
+      name: 'Rachão LeEACH',
+      description: 'Partida legendária',
+      address: 'USP Leste',
+      level: "To infinity and beyond",
+      user: user
+    )
+  end
+
   it 'invalido sem nome' do
     match = Match.new
     match.name = ""
     expect(match).not_to be_valid
   end
+
   it 'invalido sem descricao' do
     match = Match.new
     match.description = ""
     expect(match).not_to be_valid
   end
+
   it 'invalido sem endereco' do
     match = Match.new
     match.address = ""
     expect(match).not_to be_valid
   end
+
   it 'invalido sem nivel' do
     match = Match.new
     match.level = ""
     expect(match).not_to be_valid
+  end
+
+  it 'não é válido sem criador' do
+    match = Match.new
+    match.user_id = nil
+    expect(match).not_to be_valid   match = Match.new
+  end
+
+  it 'é válido com atributos válidos' do
+    expect(@match).to be_valid
   end
 
   it 'realiza pesquisa' do
