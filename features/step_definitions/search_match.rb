d30b95ff33af8e0676444818e5e603c9c3ff7036
Thais Lasso <thais.lasso@gmail.com>
2022-10-25 22:01:39 -0300

    fix: cucumber tests

--- features/step_definitions/search_match.rb
+++ features/step_definitions/search_match.rb
@@ -1,7 +1,8 @@
 Given('I open the matches page') do
-    @match0 = Match.create(name: 'Rachão da EACH', description: 'Description0', address: 'USP Leste', level: "Beginner")
-    @match1 = Match.create(name: 'Rachão do ICMC', description: 'Description1', address: 'Cidade Universitária', level: "Beginner")
-    @match2 = Match.create(name: 'Rachão do IME', description: 'Description2', address: 'USP São Carlos', level: "Beginner")
+    user = User.create(name: 'LeBron James', email: 'lebron@james.com', password: '123456', password_confirmation: '123456', birth_date: Date.new(1984, 12, 30), position: 'Power Forward')
+    @match0 = Match.create(name: 'Rachão da EACH', description: 'Description0', address: 'USP Leste', level: "Beginner", user: user)
+    @match1 = Match.create(name: 'Rachão do ICMC', description: 'Description1', address: 'Cidade Universitária', level: "Beginner", user: user)
+    @match2 = Match.create(name: 'Rachão do IME', description: 'Description2', address: 'USP São Carlos', level: "Beginner", user: user)
 
     visit '/matches'
 end
