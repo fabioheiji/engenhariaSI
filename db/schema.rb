91fdcc393108b957d8e1bf5556ff6b3d84eb74a8
Thais Lasso <thais.lasso@gmail.com>
2022-10-28 18:59:38 -0300

    fix: update migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_132524) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_204101) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_132524) do
     t.string "level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "matches_users", id: false, force: :cascade do |t|
