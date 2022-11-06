ee603837a81b31bf6866dfaaaccbf96aafe38377
Fabio <fabioheiji@gmail.com>
2022-11-06 16:50:57 -0300

    fix: remove a old test that doesn't make sense anymore because now the user doesnt have the option to accidentally join the same match more than once

--- spec/requests/matches_spec.rb
+++ spec/requests/matches_spec.rb
@@ -72,18 +72,11 @@ RSpec.describe "Matches", type: :request do
       expect(response).to have_http_status(:redirect)
     end
     
-    it "should not redirect" do
-      get "/matches/#{@match.id}"
-      post create_participate_in_match_path, params: {participate_in_match: {user_id: @user.id, match_id: @match.id}}
-      post create_participate_in_match_path, params: {participate_in_match: {user_id: @user.id, match_id: @match.id}}
-      expect(response).not_to have_http_status(:redirect)
-    end
-    
     it "should remove the user from a match" do
       get "/matches/#{@match.id}"
       post create_participate_in_match_path, params: {participate_in_match: {user_id: @user.id, match_id: @match.id}}
       post create_participate_in_match_path, params: {participate_in_match: {user_id: @user.id, match_id: @match.id}, commit: "Sair da Partida"}
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:redirect)
     end
 
   end
