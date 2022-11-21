ab3ecbb5dbb5ef4662dd1cf5bf852d9c8969e605
Thais Lasso <thlasso@pop-os.localdomain>
2022-11-20 21:42:33 -0300

    test: unit tests for match model

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -65,14 +65,56 @@ RSpec.describe Match, type: :model do
   end
 
   it 'retorna todas as partidas' do
-    match0 = Match.create(name: 'Rachão da EACH', description: 'Description0', address: 'USP Leste', level: "Livre", limit: 15) 
-    match1 = Match.create(name: 'Rachão do ICMC', description: 'Description1', address: 'Cidade Universitária', level: "Livre", limit: 15)
-    match2 = Match.create(name: 'Rachão do IME', description: 'Description2', address: 'USP São Carlos', level: "Livre", limit: 15)
+    match0 = Match.create(name: 'Rachão da EACH', description: 'Description0', address: 'USP Leste', level: 'Livre', halfCourt: 0, starts_at: '2020-11-05T15:00', limit: 15, user: @user)
+    match1 = Match.create(name: 'Rachão do ICMC', description: 'Description1', address: 'Cidade Universitária', level: 'Iniciante', halfCourt: 1, starts_at: '2021-11-05T15:00', limit: 10, user: @user)
+    match2 = Match.create(name: 'Rachão do IME', description: 'Description2', address: 'USP São Carlos', level: 'Iniciante', halfCourt: 0, starts_at: '2022-11-05T15:00', limit: 4, user: @user)
     
     matches = Match.all
     results = Match.search(nil)
 
     expect(results).to all(be_valid)
     expect(results).to eq(matches)
+  end
+
+  it 'filtra partidas por data e hora de inicio' do
+    match0 = Match.create(name: 'Rachão da EACH', description: 'Description0', address: 'USP Leste', level: 'Livre', halfCourt: 0, starts_at: '2020-11-05T15:00', limit: 15, user: @user)
+    match1 = Match.create(name: 'Rachão do ICMC', description: 'Description1', address: 'Cidade Universitária', level: 'Iniciante', halfCourt: 1, starts_at: '2021-11-05T15:00', limit: 10, user: @user)
+    match2 = Match.create(name: 'Rachão do IME', description: 'Description2', address: 'USP São Carlos', level: 'Iniciante', halfCourt: 0, starts_at: '2022-11-05T15:00', limit: 4, user: @user)
+
+    matches = [match1, match2]
+    result = Match.filter_by_starts_at("2021-11-05T15:00")
+    expect(result).to eq(matches)
+  end
+
+  it 'filtra partidas por limite de participantes' do
+    match0 = Match.create(name: 'Rachão da EACH', description: 'Description0', address: 'USP Leste', level: 'Livre', halfCourt: 0, starts_at: '2020-11-05T15:00', limit: 15, user: @user)
+    match1 = Match.create(name: 'Rachão do ICMC', description: 'Description1', address: 'Cidade Universitária', level: 'Iniciante', halfCourt: 1, starts_at: '2021-11-05T15:00', limit: 10, user: @user)
+
+    result = Match.filter_by_limit(15)
+    expect(result).to eq([match0])
+  end
+
+  it 'filtra partidas por nível de difuculdade' do
+    match0 = Match.create(name: 'Rachão da EACH', description: 'Description0', address: 'USP Leste', level: 'Livre', halfCourt: 0, starts_at: '2020-11-05T15:00', limit: 15, user: @user)
+    match1 = Match.create(name: 'Rachão do ICMC', description: 'Description1', address: 'Cidade Universitária', level: 'Iniciante', halfCourt: 1, starts_at: '2021-11-05T15:00', limit: 10, user: @user)
+
+    result = Match.filter_by_level("Livre")
+    expect(result).to eq([match0])
+  end
+
+  it 'filtra partidas que apenas são meia quadra' do
+    match0 = Match.create(name: 'Rachão da EACH', description: 'Description0', address: 'USP Leste', level: 'Livre', halfCourt: 0, starts_at: '2020-11-05T15:00', limit: 15, user: @user)
+    match1 = Match.create(name: 'Rachão do ICMC', description: 'Description1', address: 'Cidade Universitária', level: 'Iniciante', halfCourt: 1, starts_at: '2021-11-05T15:00', limit: 10, user: @user)
+
+    result = Match.filter_by_half_court(true)
+    expect(result).to eq([match1])
+  end
+
+  it 'filtra partidas que apenas não são meia quadra' do
+    match0 = Match.create(name: 'Rachão da EACH', description: 'Description0', address: 'USP Leste', level: 'Livre', halfCourt: 0, starts_at: '2020-11-05T15:00', limit: 15, user: @user)
+    match1 = Match.create(name: 'Rachão do ICMC', description: 'Description1', address: 'Cidade Universitária', level: 'Iniciante', halfCourt: 1, starts_at: '2021-11-05T15:00', limit: 10, user: @user)
+
+    result = Match.filter_by_half_court(false)
+    expect(result).to eq([match0])
   end
 end
